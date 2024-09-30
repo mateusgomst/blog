c11ac7b69b565b87e292f4946ed9bbff3b6f6097
Mateus Gomes <mateusgomes9119@gmail.com>
2024-09-30 19:03:33 -0300

    show one article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'articles#index' #a raiz da minha rota vai ser o articles pra action index, entao quando colcoar o local host 3000, ja vai pro articles index
-  get '/articles', to: 'articles#index' #se chegar uma rota /articles eu quero que direcione para articles#index
+  #get '/articles', to: 'articles#index' #se chegar uma rota /articles eu quero que direcione para articles#index
+  #get '/articles/:id', to: 'articles#show'
+  resources :articles #ja traz toda informação do crud, substitui as rotas acima, simplifica
+
 end
   
