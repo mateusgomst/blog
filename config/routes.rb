9c6154e35e3fb73184ccef60fac2b50fa9dd7550
Mateus Gomes <mateusgomes9119@gmail.com>
2024-09-30 17:49:36 -0300

    routes, create db, insert tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'articles#index' #a raiz da minha rota vai ser o articles pra action index, entao quando colcoar o local host 3000, ja vai pro articles index
+  get '/articles', to: 'articles#index' #se chegar uma rota /articles eu quero que direcione para articles#index
 end
+  
