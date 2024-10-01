56f2bc259e68f52db31fa85059090bfead90a71c
Mateus Gomes <mateusgomes9119@gmail.com>
2024-10-01 18:52:23 -0300

    Incremented new and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,38 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id]) # Corrigido para singular
+  end
+
+  def new
+    @article = Article.new # Corrigido para singular
+  end
+
+  def create
+    @article = Article.new(article_params) # Corrigido erro de digitação e uso de singular
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def article_params # Corrigido o comentário e formatação
+    params.require(:article).permit(:title, :body)
   end
 end
